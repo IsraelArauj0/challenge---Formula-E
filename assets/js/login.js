6fefbb0e27ee33ba648483c9ed6f628c17b27747
Paulo <paulopocas20@gmail.com>
2024-06-15 21:40:20 -0300

    adicionando login

---
+++ assets/js/login.js
@@ -0,0 +1,19 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); //Evita o envio do formulário
+
+    //Captura dos valores dos campos de entrada
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    //Verifica se os campos estão preenchidos
+    if (username.trim() === '' || password.trim() === '') {
+        alert('Por favor, preencha todos os campos.');
+        return;
+    }
+
+    //Verifica as credenciais
+    if (username === 'usuario' && password === 'senha') {
+    } else {
+        alert('Credenciais inválidas. Tente novamente.');
+    }
+});
